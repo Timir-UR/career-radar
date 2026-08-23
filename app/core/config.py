5013743a38e9897685a50104ab59c45e90d341c2
Гиндуллин Тимур <timurgindullin2005@gmail.com>
2026-08-23 23:51:26 +0300

    feat: settings via pydantic-settings

---
+++ app/core/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from urllib.parse import quote_plus
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+    POSTGRES_USER: str 
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_PORT: int = 5432
+    POSTGRES_HOST: str
+    RABBITMQ_URL: str
+
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{quote_plus(self.POSTGRES_PASSWORD)}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+    
+    @property
+    def database_url_sync(self) -> str:
+        return f"postgresql+psycopg://{self.POSTGRES_USER}:{quote_plus(self.POSTGRES_PASSWORD)}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+    
+settings = Settings()
